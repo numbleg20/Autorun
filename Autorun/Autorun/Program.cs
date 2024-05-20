91476730e44eb8a7093dc62c8d8d1ee6720ac77b
numbleg <111631956+numbleg20@users.noreply.github.com>
2024-05-20 12:13:09 +0300

    Update Program.cs

--- Autorun/Autorun/Program.cs
+++ Autorun/Autorun/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 #region using
 using Microsoft.Win32;
 using System;
@@ -21,28 +21,30 @@ namespace Autorun {
 
             if (commandLineArgs.Contains("-added")) {
                 string appPath = commandLineArgs[2];
-
                 RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(
                     "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-                registryKey.SetValue(Path.GetFileNameWithoutExtension(appPath), appPath);
-                registryKey.Close();
-                return;
+                if (!registryKey.GetValueNames().Contains(Path.GetFileNameWithoutExtension(appPath))) {
+                    registryKey.SetValue(Path.GetFileNameWithoutExtension(appPath), appPath);
+                    registryKey.Close();
+                }
+                else {
+                    registryKey.DeleteValue(Path.GetFileNameWithoutExtension(appPath));
+                    registryKey.Close();
+                }
             }
             else if (commandLineArgs.Contains("-removeReg")) {
                 RegistryKey _key = Registry.ClassesRoot.OpenSubKey("exefile\\Shell", true);
-                _key.DeleteSubKeyTree("Add to Autorun");
+                _key.DeleteSubKeyTree("Add/Remove from Autorun");
                 _key.Close();
-                return;
             }
             else {
                 RegistryKey _key = Registry.ClassesRoot.OpenSubKey("exefile\\Shell", true);
-                RegistryKey newkey = _key.CreateSubKey("Add to Autorun");
+                RegistryKey newkey = _key.CreateSubKey("Add/Remove from Autorun");
                 RegistryKey subNewkey = newkey.CreateSubKey("Command");
                 subNewkey.SetValue("", Environment.CurrentDirectory + "\\Autorun.exe -added \"%1\"");
                 subNewkey.Close();
                 newkey.Close();
                 _key.Close();
-                return;
             }
         }
     }
